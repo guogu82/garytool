e9c396726390a77bb9228078239ede0b7cc2e239
Gary <55174748@qq.com>
2015-10-23 18:09:48 +0800

    opt

--- app/src/main/java/com/gary/garytool/MainActivity.java
+++ app/src/main/java/com/gary/garytool/MainActivity.java
@@ -2,6 +2,8 @@ package com.gary.garytool;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         mFunctions = (Button) findViewById(R.id.bt_functions);
         mFunctions.setOnClickListener(new View.OnClickListener() {
             @Override
