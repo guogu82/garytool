95efd9bd65dc8ded49acc62d1c4b3c8c219d1d65
Gary <55174748@qq.com>
2015-12-30 18:28:32 +0800

    activityMenu

--- app/src/main/java/com/gary/garytool/InsaneDemoActivity.java
+++ app/src/main/java/com/gary/garytool/InsaneDemoActivity.java
@@ -1,7 +1,11 @@
 package com.gary.garytool;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 
 
 public  class InsaneDemoActivity extends Activity {
@@ -14,5 +18,14 @@ public  class InsaneDemoActivity extends Activity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        SubMenu prog=menu.addSubMenu("启动程序");
+        prog.setHeaderIcon(R.drawable.pictures_selected);
+        prog.setHeaderTitle("选择你要启动的程序");
 
+        MenuItem item=prog.add("查看Swift");
+        item.setIntent(new Intent(this,MainInsaneActivity.class));
+        return super.onCreateOptionsMenu(menu);
+    }
 }
