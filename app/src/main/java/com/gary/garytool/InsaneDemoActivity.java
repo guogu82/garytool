b80119d88ebaea16b7ba07cbdf1a50fafc0aa624
Gary <55174748@qq.com>
2016-01-15 17:13:44 +0800

    menu

--- app/src/main/java/com/gary/garytool/InsaneDemoActivity.java
+++ app/src/main/java/com/gary/garytool/InsaneDemoActivity.java
@@ -1,112 +1,51 @@
 package com.gary.garytool;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.graphics.Color;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.gary.garytool.volley.BitmapCache;
-
-import java.util.TreeMap;
+import android.widget.ArrayAdapter;
 
 
-public  class InsaneDemoActivity extends Activity {
+public  class InsaneDemoActivity extends Activity implements ActionBar.OnNavigationListener {
 
 
-    private TextView txt;
-
+    private static final String SELECTED_ITEM="selected_item";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insane_demo);
-        txt= (TextView) findViewById(R.id.txt);
 
-        registerForContextMenu(txt);
+        final ActionBar actionBar=getActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        actionBar.setListNavigationCallbacks(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,android.R.id.text1,new String[]{"第一页","第二页","第三页"}),this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater=new MenuInflater(this);
-        inflater.inflate(R.menu.menu_main,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-
-        MenuInflater inflater=new MenuInflater(this);
-        inflater.inflate(R.menu.context,menu);
-        menu.setHeaderIcon(R.drawable.pictures_selected);
-        menu.setHeaderTitle("选择背景色");
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        switch (item.getItemId())
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if(savedInstanceState.containsKey(SELECTED_ITEM))
         {
-            case R.id.red:
-                item.setChecked(true);
-                txt.setBackgroundColor(Color.RED);
-                break;
-            case R.id.green:
-                item.setChecked(true);
-                txt.setBackgroundColor(Color.GREEN);
-                break;
-            case R.id.blue:
-                item.setChecked(true);
-                txt.setBackgroundColor(Color.BLUE);
-                break;
+            getActionBar().setSelectedNavigationItem(savedInstanceState.getInt(SELECTED_ITEM));
         }
-        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.isCheckable())
-        {
-            item.setChecked(true);
-        }
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM,getActionBar().getSelectedNavigationIndex());
+    }
 
-        switch (item.getItemId())
-        {
-            case R.id.font_10:
-                txt.setTextSize(10*2);
-                break;
-            case R.id.font_12:
-                txt.setTextSize(12*2);
-                break;
-            case R.id.font_14:
-                txt.setTextSize(14*2);
-                break;
-            case R.id.font_16:
-                txt.setTextSize(16*2);
-                break;
-            case R.id.font_18:
-                txt.setTextSize(18*2);
-                break;
-            case R.id.red_font:
-                txt.setTextColor(Color.RED);
-                item.setChecked(true);
-                break;
-            case R.id.green_font:
-                txt.setTextColor(Color.GREEN);
-                item.setChecked(true);
-                break;
-            case  R.id.blue_font:
-                txt.setTextColor(Color.BLUE);
-                item.setChecked(true);
-                break;
-            case R.id.plain_item:
-                Toast toast=Toast.makeText(InsaneDemoActivity.this,"您单击了普通菜单项",Toast.LENGTH_SHORT);
-                toast.show();
-                break;
-        }
+    @Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        Fragment fragment=new DummyFragment();
+        Bundle args=new Bundle();
+        args.putInt(DummyFragment.ARG_SECTION_NUMBER,itemPosition+1);
+        fragment.setArguments(args);
+        FragmentTransaction ft=getFragmentManager().beginTransaction();
+        ft.replace(R.id.container,fragment);
+        ft.commit();
         return true;
     }
 }
