67c5e7d64c719ae6a534719e5d0105949140cdc7
Gary <55174748@qq.com>
2016-02-17 18:16:07 +0800

    custom view

--- app/src/main/java/com/gary/garytool/InsaneDemoActivity.java
+++ app/src/main/java/com/gary/garytool/InsaneDemoActivity.java
@@ -3,13 +3,35 @@ package com.gary.garytool;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+
 
 public  class InsaneDemoActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_insane_demo);
+        Button bn= (Button) findViewById(R.id.bn);
+        bn.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction()==KeyEvent.ACTION_DOWN)
+                {
+                    Log.v("-Listener","the onKeyDown inListener");
+                }
+                return true;
+            }
+        });
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        super.onKeyDown(keyCode,event);
+        Log.v("-Activity","the onKeyDown in Activity");
+        return false;
+    }
 }
