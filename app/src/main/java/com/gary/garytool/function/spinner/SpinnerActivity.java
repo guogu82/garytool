89ec76c3e031ee024718baa465ccc36e5e65fbe2
gary <gu.guo@ctauto.cn>
2016-06-28 20:52:47 +0800

    base spinner

--- app/src/main/java/com/gary/garytool/function/spinner/SpinnerActivity.java
+++ app/src/main/java/com/gary/garytool/function/spinner/SpinnerActivity.java
@@ -2,22 +2,10 @@ package com.gary.garytool.function.spinner;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.gary.garytool.R;
-import com.gary.garytool.lib.fancyindexer.adapter.CarBrandAdapter;
-import com.gary.garytool.lib.fancyindexer.domain.CarBrand;
-import com.gary.garytool.lib.fancyindexer.domain.Cheeses;
-import com.gary.garytool.lib.fancyindexer.domain.GoodMan;
-import com.gary.garytool.lib.fancyindexer.ui.FancyIndexer;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by Gary on 2016/6/15/015.
@@ -28,6 +16,17 @@ public class SpinnerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.function_activity_spinner);
 
+        Spinner spinner= (Spinner) findViewById(R.id.spinner_normal);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SpinnerActivity.this,android.R.layout.simple_spinner_item);
+        adapter.add(" 上海 ");
+        adapter.add(" 北京 ");
+        adapter.add(" 南京 ");
+        adapter.add(" 哈尔滨 ");
+        adapter.add(" 乌鲁木齐 ");
+        adapter.add(" 符拉迪沃斯托克 ");
+        adapter.add(" 圣弗朗西斯科 ");
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spinner.setAdapter(adapter);
 
     }
 }
